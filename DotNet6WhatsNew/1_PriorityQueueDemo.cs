1f5613755232b039161292e54483faa16cad75ca
Milan Milas <milanmilas@gmail.com>
2021-12-27 20:06:12 +0000

    Add project files.

---
+++ DotNet6WhatsNew/1_PriorityQueueDemo.cs
@@ -0,0 +1,28 @@
+﻿namespace DotNet6WhatsNew;
+
+public static class PriorityQueueDemo
+{
+    public static void Run()
+    {
+        var queue = new PriorityQueue<object, int>();
+        queue.Enqueue("first", 2);
+        queue.Enqueue("second", 1);
+        queue.Enqueue("3rd", 10);
+
+        var element = queue.Dequeue();
+        Console.WriteLine($"dequeue top priority element '{element}'");
+        // "second"
+
+        var queue2 =
+            new PriorityQueue<string, string>(
+                Comparer<string>.Create(
+                    (x, y) => x.Length > y.Length ? 1 : -1));
+        queue2.Enqueue("first", "first");
+        queue2.Enqueue("second", "second");
+        queue2.Enqueue("3rd", "3rd");
+
+        element = queue2.Dequeue();
+        Console.WriteLine($"dequeue2 top priority element '{element}'");
+        // "3rd"
+    }
+}
---
+++ DotNet6WhatsNew/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNet6WhatsNew;
+
+Console.WriteLine("Hello, World!");
+
+PriorityQueueDemo.Run();
+
+Console.ReadKey();
